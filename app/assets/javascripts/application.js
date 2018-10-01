7f903a41577a3fb456acf9e19f392e56f88e9fca
alemat <alematgebru@gmail.com>
2018-10-01 17:34:52 +0300

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,43 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require adminbsb/plugins/jquery/dist/jquery.min
+//= require adminbsb/plugins/jquery-ui/jquery-ui
+//= require adminbsb/plugins/bootstrap/dist/js/bootstrap.min
+//= require adminbsb/plugins/pace/pace
+//= require adminbsb/plugins/screenfull/src/screenfull
+//= require adminbsb/plugins/metisMenu/dist/metisMenu
+//= require adminbsb/plugins/jquery-slimscroll/jquery.slimscroll
+//= require adminbsb/plugins/switchery/dist/switchery
+//= require adminbsb/plugins/iCheck/icheck
+//= require adminbsb/plugins/jquery-sparkline/dist/jquery.sparkline
+//= require adminbsb/plugins/flot/jquery.flot
+//= require adminbsb/plugins/flot-spline/js/jquery.flot.spline
+//= require adminbsb/plugins/flot/jquery.flot.pie
+//= require adminbsb/plugins/flot/jquery.flot.categories
+//= require adminbsb/plugins/flot/jquery.flot.time
+//= require adminbsb/plugins/DataTables/media/js/jquery.dataTables
+//= require adminbsb/plugins/DataTables/media/js/dataTables.bootstrap
+//= require adminbsb/plugins/DataTables/extensions/export/dataTables.buttons.min
+//= require adminbsb/plugins/DataTables/extensions/export/buttons.bootstrap.min
+//= require adminbsb/plugins/DataTables/extensions/export/buttons.flash.min
+//= require adminbsb/plugins/DataTables/extensions/export/jszip.min
+//= require adminbsb/plugins/DataTables/extensions/export/pdfmake.min
+//= require adminbsb/plugins/DataTables/extensions/export/vfs_fonts
+//= require adminbsb/plugins/DataTables/extensions/export/buttons.html5.min
+//= require adminbsb/plugins/DataTables/extensions/export/buttons.print.min
+//= require adminbsb/plugins/peity/jquery.peity
+//= require adminbsb/js/admin
+//= require adminbsb/js/pages/dashboard/dashboard
+//= require adminbsb/js/google-analytics
+//= require adminbsb/js/demo
+//= require activestorage
